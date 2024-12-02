2ab1d225cbc607910f68c0ccae226d6d057fb951
Davi Fernandes <davi.fer159@gmail.com>
2024-12-02 09:59:12 -0300

    responsivo finalizado

--- js/main.js
+++ js/main.js
@@ -2,4 +2,21 @@ var swiper = new Swiper(".slide-characters", {
   slidesPerView: 3.5,
   spaceBetween: 19,
   fremode: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+    },
+    540: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 2.2,
+    },
+    991: {
+      slidesPerView: 2.8,
+    },
+    1200: {
+      slidesPerView: 3.5,
+    }
+  }
 });
